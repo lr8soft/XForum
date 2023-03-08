79fdfa6af0a0ad1a9e823129a5c35e1b3105e125
LT_lrsoft <lrsoft@hotmail.com>
2023-03-08 10:03:51 +0800

    v0.11

--- server/client/src/main.js
+++ server/client/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersist from "pinia-plugin-persist";
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/theme-chalk/index.css'
@@ -9,6 +10,8 @@ import 'element-plus/theme-chalk/index.css'
 import router from "@/router";
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersist);
+
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
--- server/client/src/services/globalData.js
+++ server/client/src/services/globalData.js
@@ -1,20 +1,26 @@
 import {defineStore} from "pinia";
-
 export const useGlobalData = defineStore('globaldata',{
     state: () => ({
         is_login: false,
-        user_token: ''
     }),
+    persist: {
+        enabled: true,
+        strategies: [{
+            storage: localStorage,
+            paths: ['is_login'],
+            key: "is_login"
+        },{
+            storage: sessionStorage,
+            paths: ['is_login'],
+            key: "is_login"
+        }],
+    },
     getters: {
-        isLogin: (state) => state.is_login,
-        token: (state) => state.user_token
+        isLogin: (state) => state.is_login
     },
     actions: {
         setIsLogin(value){
             this.is_login = value
-        },
-        setToken(value){
-            this.user_token = value
         }
     }
 })
